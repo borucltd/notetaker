3142fd0e67653b22beb47bfc21c642923e934593
borucltd <mboruc@DESKTOP-L3J83AR.localdomain>
2020-05-06 22:11:56 +0930

    HTML routing added.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
 // ===========================================================================
 // SERVER 
@@ -15,8 +16,22 @@ server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
 // ================================================================================
-// ROUTER
+// ROUTING
 // ================================================================================
+
+
+// http:/X.X.X.X:YYYY/notes ==> ./public/notes.html
+server.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+
+// http:/X.X.X.X:YYYY/* ==> ./public/index.html
+server.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+
+
 
 
 // =============================================================================
