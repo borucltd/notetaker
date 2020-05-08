f34a36f29d0b75274157cf057fe8b394228b2eb4
borucltd <mboruc@DESKTOP-L3J83AR.localdomain>
2020-05-08 21:57:01 +0930

    Adding new notes is working.

--- local_modules/apiRoutes.js
+++ local_modules/apiRoutes.js
@@ -6,10 +6,10 @@ const fs = require('fs');
 
 // Reference to our database
 const dbFile = path.join(__dirname, "../db/db.json");
-const inMemoryDb = {};
+
 
 // Callback which reads database
-function readDatabase(req, res) {
+function readDatabase(req, res, next) {
 
   console.log(dbFile);
 
@@ -21,26 +21,55 @@ function readDatabase(req, res) {
      console.log(`ERROR: Database reading error ${err}`);
      throw err;
     }
-
+   
     // we need to parse raw data into JSON
     let records = JSON.parse(data);
-    console.log(records);
     res.json(records);   
     
   });  
 }
 
 // Callback which writes to database
-function addToDatabase(req, res, next) {
+function addToDatabase(req, res) {
+  
+  console.log(`INFO: reading database ${dbFile}`);
+  fs.readFile(dbFile, (err, data) => {
+    
+    if (err) {
+     console.log(`ERROR: Database reading error ${err}`);
+     throw err;
+    }
+   
+    // we need to parse raw data into JSON
+    let records = JSON.parse(data);
+        
+    // add "id" attribute to a new record
+    req.body.id = records.length;
+    
+    // add new record to existing ones
+    records.push(req.body);
 
-  console.log(dbFile);
-  console.log(req.body);
+    // save to a file
+    console.log(`INFO: adding to database ${dbFile}`);
+    fs.writeFile(dbFile, JSON.stringify(records) , (err) => {
+      
+      if (err) {
+       console.log(`ERROR: Database writing error ${err}`);
+       throw err;
+      }
+      
+      // once database is updated we are sending new request back to the client
+      res.json(req.body); 
+    });
+  });   
+
 }
 
 // Callback which writes to database
 function deleteFromDatabase(req, res,next) {
 
   console.log(req.body);
+  res.json(true);
 }
 
 // Function which does the routing for API requests
@@ -51,7 +80,7 @@ function apiRoutes(server) {
    
   // POST `/api/notes` - Should receive a new note to save on the request body, 
   // add it to the `db.json` file, and then return the new note to the client.
-  server.post("/api/notes", [addToDatabase] );
+  server.post("/api/notes", addToDatabase );
 
   // * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.
   server.delete("/api/notes/*", [deleteFromDatabase] );
